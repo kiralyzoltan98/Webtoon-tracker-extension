fd94e63943ac4746413c9eaaff20960f31109fbc
kiralyzoltan98 <kiralyzoltan98@gmail.com>
2022-07-19 22:36:36 +0200

    Can read every changeing url, needs frontend for storing data in three lists, it will be storen in local storage in the "back-end"

--- background.js
+++ background.js
@@ -1,7 +1,24 @@
 
 let color = '#3aa757';
 
+let json = fetch('./template.json').then(results => results.json()).then(console.log);
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+    //console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        let data;
+        if ("webtoonTracker" in localStorage){
+            data = JSON.parse(localStorage.getItem("webtoonTracker"));
+        } else {
+            localStorage.setItem('webtoonTracker', json);
+            data = json;
+        }
+
+        if (changeInfo.url) {
+            console.log("Changinfo data: ", changeInfo.url);
+        }
+    }
+);
