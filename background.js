fb25b83c38602d695971f357d0ef0b995bd2d415
kiralyzoltan98 <kiralyzoltan98@gmail.com>
2022-08-03 22:13:07 +0200

    nearly finished, needs add button, and figure out how to refresh options page from background.js

--- background.js
+++ background.js
@@ -12,9 +12,6 @@ let json = {
 
 chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
 
-
-
-
         if (changeInfo.url) {
             let data = await chrome.storage.sync.get(["webtoonTracker"])
 
@@ -23,6 +20,21 @@ chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
             } else {
                 await chrome.storage.sync.set({webtoonTracker: json});
                 data = json;
+            }
+
+            //if changeInfo.url contains any key from data.webtoonTracker then update its value
+            for (let key in data.webtoonTracker) {
+                for (let key2 in data.webtoonTracker[key]) {
+                    let title = key2.replace(/\s/g , "-") + "-chapter-";
+                    let regex = new RegExp( "(?<=" + title + ")([0-9]*?)(?=\.html)", "g");
+                    console.log("regex: ", regex);
+                    let chapter = changeInfo.url.match(regex);
+                    console.log("chapter: ", chapter[0]);
+                    if (chapter[0] > data.webtoonTracker[key][key2]) {
+                        data.webtoonTracker[key][key2] = chapter[0];
+                        chrome.storage.sync.set({webtoonTracker: data});
+                    }
+                }
             }
             console.log("Changinfo data: ", changeInfo.url);
         }
--- options.js
+++ options.js
@@ -1,4 +1,34 @@
 
+//every second, check if the data has changed and if so, update the table
+setInterval(function () {
+    chrome.storage.sync.get(["webtoonTracker"], function (data) {
+        if (data) {
+            updateTable(data.webtoonTracker);
+        }
+    });
+}, 100);
+
+//function to update the table
+function updateTable(data) {
+    let tableData = document.getElementById("container");
+    let table = tableData.getElementsByTagName("table");
+    for (let i = 0; i < table.length; i++) {
+        let tableBody = table[i].getElementsByTagName("tbody")[0];
+        let tableHeader = table[i].getElementsByTagName("thead")[0];
+        let tableHeaderText = tableHeader.getElementsByTagName("th")[0].innerText;
+        let tableRows = tableBody.getElementsByTagName("tr");
+        for (let j = 0; j < tableRows.length; j++) {
+            let tableCells = tableRows[j].getElementsByTagName("td");
+            let tableCellText = tableCells[0].getElementsByTagName("input")[0].value;
+            let tableCellValue = tableCells[1].getElementsByTagName("input")[0].value;
+            if (data[tableHeaderText][tableCellText]) {
+                tableCells[1].getElementsByTagName("input")[0].value = data[tableHeaderText][tableCellText];
+            }
+        }
+    }
+}
+
+
 chrome.storage.sync.get("webtoonTracker", (data) => {
     let tableData = data.webtoonTracker;
 
@@ -10,16 +40,6 @@ chrome.storage.sync.get("webtoonTracker", (data) => {
             json[key][key2] = tableData[key][key2];
         }
     }
-
-    //create save button, ctrl+s to save
-    let saveButton = document.createElement("button");
-    saveButton.innerText = "Save";
-    saveButton.id = "saveButton";
-    saveButton.addEventListener("click", async () => {
-        let data = await chrome.storage.sync.set({webtoonTracker: json});
-        console.log("saved: ", $("#container :input"));
-    });
-    document.body.appendChild(saveButton);
 
     Object.keys(tableData).forEach(key => {
         let table = document.getElementById(key);
@@ -48,3 +68,43 @@ chrome.storage.sync.get("webtoonTracker", (data) => {
         table.appendChild(tableBody);
     });
 });
+
+//call saveData function on click of save button
+document.getElementById("save").addEventListener("click", saveData);
+
+
+
+//function to save data on click of save button
+function saveData() {
+    // let json = {
+    //     Webtoon: {
+    //         write: "1"
+    //     },
+    //     Manga: {
+    //         your: "2"
+    //     },
+    //     Anime: {
+    //         titles: "3"
+    //     }
+    // }
+    // chrome.storage.sync.set({webtoonTracker: json});
+
+    let data = {};
+    let tableData = document.getElementById("container");
+    let table = tableData.getElementsByTagName("table");
+    for (let i = 0; i < table.length; i++) {
+        let tableBody = table[i].getElementsByTagName("tbody")[0];
+        let tableHeader = table[i].getElementsByTagName("thead")[0];
+        let tableHeaderText = tableHeader.getElementsByTagName("th")[0].innerText;
+        data[tableHeaderText] = {};
+        let tableRows = tableBody.getElementsByTagName("tr");
+        for (let j = 0; j < tableRows.length; j++) {
+            let tableCells = tableRows[j].getElementsByTagName("td");
+            let tableCellText = tableCells[0].getElementsByTagName("input")[0].value;
+            let tableCellValue = tableCells[1].getElementsByTagName("input")[0].value;
+            data[tableHeaderText][tableCellText] = tableCellValue;
+        }
+    }
+    console.log("data: ", data);
+    chrome.storage.sync.set({webtoonTracker: data});
+}
